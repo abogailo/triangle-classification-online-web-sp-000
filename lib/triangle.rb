9671670401e95ef22fcc808ddda3bee4d8fdef22
Abagail Hudson <abagailanne@gmail.com>
2019-06-02 20:15:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,26 +2,26 @@ class Triangle
   # write code here
   attr_accessor :length1, :length2, :length3
 
-def initialize(length1, length2, length3)
-  @length1 = length1
-  @length2 = length2
-  @length3 = length3
-end
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
 
-def kind
-  low, mid, max = [@length1, @length2, @length3].sort
-    if  low + mid = max
-      raise TriangleError
-    elsif low == 0 || mid == 0 || max == 0
-      raise TriangleError
+    def kind
+      low, mid, max = [@length1, @length2, @length3].sort
+      if  low + mid = max
+        raise TriangleError
+      elsif low == 0 || mid == 0 || max == 0
+        raise TriangleError
+      end
+      
+      if @length1 == @length2 && @length2 == length3
+        :equilateral
+      elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+        :isosceles
+      else
+        :scalene
+      end
     end
-end
-if @length1 == @length2 && @length2 == length3
-      :equilateral
-    elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
-      :isosceles
-    else
-      :scalene
-    end
-
 end
