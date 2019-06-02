847119ef7d09a1d6faf92bdf8c90e331ba5ed520
Abagail Hudson <abagailanne@gmail.com>
2019-06-02 20:12:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  attr_accessor :length1, :length2, :length3
+
+def initialize(length1, length2, length3)
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
+end
+
+def kind
+  low, mid, max = [@length1, @length2, @length3].sort
+    if  low + mid = max
+      raise TriangleError
+    elsif low || mid || max == 0
+      raise TriangleError
+    end
+end
+
+
 end
